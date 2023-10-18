dc1a3f40df594610a973cb8671f5dc99f96f0882
wanqingying <wanqingying@outlook.com>
2023-10-18 23:49:41 +0800

    init v11

--- zna/hello.js
+++ zna/hello.js
@@ -1,13 +1,13 @@
 
 var addon;
 
-if (process.platform == 'win32') {
-    addon = require('./hello-win.node');
+if (process.platform === 'win32') {
+    addon = require('./hello-win-64.node');
 }
-else if (process.platform == 'darwin') {
-   throw new Error('darwin is not supported');
+else if (process.platform === 'darwin') {
+   addon = require('./hello-linux-arm-64');
 }else{
-    addon = require('./hello-linux.node');
+    addon = require('./hello-linux-x86-64');
 }
 
 console.log(addon.hello()); // 'world'
