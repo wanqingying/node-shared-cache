11318ea37e64116d3f2142c3a31b690a981fadea
wanqingying <wanqingying@kuaishou.com>
2023-10-19 22:07:26 +0800

    release 0.6

--- target/addon.js
+++ target/addon.js
@@ -4,9 +4,9 @@ var addon;
 if (process.platform === 'win32') {
     addon = require('./hello-win-64.node');
 } else if (process.platform === 'darwin') {
-    addon = require('./hello-linux-arm-64');
+    addon = require('./hello-linux-arm-64.node');
 } else {
-    addon = require('./hello-linux-x86-64');
+    addon = require('./hello-linux-x86-64.node');
 }
 
 class ShmCache {
