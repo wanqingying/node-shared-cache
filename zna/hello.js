44b593285314e69b5203535a7fde6bf975023ba9
wanqingying <wanqingying@kuaishou.com>
2023-10-19 10:11:50 +0800

    init v12

--- zna/hello.js
+++ zna/hello.js
@@ -11,6 +11,11 @@ else if (process.platform === 'darwin') {
 }
 
 console.log(addon.hello()); // 'world'
+cache = new addon.NodeShareCache("Highscorev4", 1024 * 100, true);
+cache.setMaxAge(4000);
+cache.setLock(true);
+cache.set("key1", "value1");
+console.log(cache.get("key1"));
 
 module.exports=addon;
 
